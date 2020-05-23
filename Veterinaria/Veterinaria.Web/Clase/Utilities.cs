bcc61e66a5110889f7789b4750ea86e3da404410
Yudith Rubio <yudiht17@gmail.com>
2020-05-22 19:16:41 -0700

    Vistas personalizadas

--- Veterinaria/Veterinaria.Web/Clase/Utilities.cs
+++ Veterinaria/Veterinaria.Web/Clase/Utilities.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,6 +11,39 @@ namespace Veterinaria.Web.Clase
     public class Utilities
     {
         readonly static ApplicationDbContext db = new ApplicationDbContext();
+        public static void CheckRoles(string roleName)
+        {
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(db));
+
+            if (!roleManager.RoleExists(roleName))
+            {
+                roleManager.Create(new IdentityRole(roleName));
+            }
+        }
+        internal static void CheckSuperUser()
+        {
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+            var userAsp = userManager.FindByName("yudiht17@gmail.com");
+
+            if (userAsp == null)
+            {
+                CreateUserASP("yudiht17@gmail.com", "admin1234", "Admin");
+            }
+        }
+
+        private static void CreateUserASP(string email, string password, string rol)
+        {
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+            var userASP = new ApplicationUser()
+            {
+                UserName = email,
+                Email = email,
+            };
+
+            userManager.Create(userASP, password);
+            userManager.AddToRole(userASP.Id, rol);
+        }
+
         public void Dispose()
         {
             db.Dispose();
